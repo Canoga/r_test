9bd92a158b182a6a4ceb931d009cc75f9f5bf6bc
Canoga <91224366+Canoga@users.noreply.github.com>
2023-01-08 22:21:55 +0100

    Finished a, without colors in diagram

--- AS4_Papa Sketch.R
+++ AS4_Papa Sketch.R
@@ -6,9 +6,9 @@ sel_dates <- subset(dataraw, date == c('2022-12-10','2022-08-01'))
 ren <- c()
 for (one_source in sel_dates[[4]]) {
   if (one_source %in% c("Biomasse", "Wasserkraft", "Wind_Offshore", "Wind_Onshore", "Photovoltaik", "Sonstige_Erneuerbare_Energien")) {
-    ren <- append (ren,TRUE)
+    ren <- append (ren,"renewable energy")
   } else {
-    ren <- append(ren,FALSE)
+    ren <- append(ren,"non-renewable energy")
   }
 }
 sel_dates$renewable = ren
@@ -29,8 +29,9 @@ for (one_source in sel_dates[[4]]) {
 sel_dates$source = ren
 
 disp_data <- sel_dates[,c(1,4,5,6)]
-disp_data <- subset(disp_data, disp_data$date == '2022-08-01')
-aggr <- aggregate(x=disp_data[,colnames(disp_data) !="source"], by=list(disp_data$source), FUN=mean)
+
+# aggr <- aggregate(x=disp_data[,colnames(disp_data) !="source"], by=list(disp_data$date,disp_data$renewable,disp_data$source), FUN=mean)
+aggr <- aggregate(x=disp_data[,colnames(disp_data) == "energy"], by=list(disp_data$date,disp_data$renewable,disp_data$source), FUN=mean)
 
 library("ggplot2")
-ggplot(data = aggr) + geom_bar(mapping = aes(x=Group.1, y=energy), stat = "identity") + coord_flip()
+ggplot(data = aggr) + geom_bar(color = "darkred", fill = "darkred", mapping = aes(x=Group.3, y=energy), stat = "identity") + coord_flip() + facet_grid(cols=vars(aggr$Group.1), rows=vars(aggr$Group.2), scales = "free") + labs(y="Energieerzeugung in [MWh]",x="")
