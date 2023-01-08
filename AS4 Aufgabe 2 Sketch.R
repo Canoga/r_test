2dc497606a1677d9b0429f965a05b5d1d1f8999d
Canoga <91224366+Canoga@users.noreply.github.com>
2023-01-09 00:55:36 +0100

    Aufgabe (a), aber mit Labels noch rechts

---
+++ AS4 Aufgabe 2 Sketch.R
@@ -0,0 +1,15 @@
+library(readr)
+library("ggplot2")
+
+
+pricesraw <- read_delim("Handelspreise_per_hour.csv", 
+                                     delim = ";", escape_double = FALSE, col_types = cols(Date = col_date(format = "%d.%m.%Y")), 
+                                     locale = locale(date_names = "de", decimal_mark = ",", 
+                                                     grouping_mark = "."), trim_ws = TRUE)
+pricesraw$StartShow <- format(as.POSIXct(pricesraw$Start), format = "%H:%M")
+
+sel_dates <- subset(pricesraw, (Date == '2022-02-24' | Date == '2022-06-14' | Date == '2022-12-05'), select = c(Date, StartShow, Deutschland))
+ggplot(data = sel_dates, aes(x=StartShow, y=Deutschland, group = 1)) + geom_line(colour = "blue") + facet_grid(rows=vars(sel_dates$Date), scales = "fixed") + labs(y="Preis",x="Uhrzeit",title = "Deutschland") + theme(axis.text.x = element_text(angle = 90))
+
+sel_dates <- subset(pricesraw, Date == '2022-02-24', select = c(Date, StartShow, Deutschland))
+ggplot(data = sel_dates, aes(x=StartShow, y=Deutschland, group = 1)) + geom_line()
